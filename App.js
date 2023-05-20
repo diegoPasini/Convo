373a07c2e9c55b2a30a17884211226a0ba520c37
otaylor2023 <75337325+otaylor2023@users.noreply.github.com>
2023-05-19 23:05:27 -0400

    whisper still broke but recording works

--- App.js
+++ App.js
@@ -5,12 +5,15 @@ import generateResponseText from './api/gpt';
 import transcribeAudio from './api/whisper';
 import { TEST_VAR } from "@env";
 import "react-native-url-polyfill/auto"
+import { Audio } from "expo-av";
 
 
 export default function App() {
 
   const [promptInput, setPromptInput] = useState("");
   const [result, setResult] = useState("Result");
+  const [recording, setRecording] = useState();
+  const [latestUri, setLatestUri] = useState();
 
   const [transcript, setTranscript] = useState("Transcript");
 
@@ -29,7 +32,7 @@ export default function App() {
         throw new Error(`Request failed`);
       }
 
-      setResult(data.message);
+      setResult(data.text);
       // setPromptInput("");
     } catch(error) {
       // Consider implementing your own error handling logic here
@@ -38,13 +41,43 @@ export default function App() {
     }
   }
 
+  async function startRecording() {
+    try {
+      console.log('Requesting permissions..');
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+
+      console.log('Starting recording..');
+      const { recording } = await Audio.Recording.createAsync( Audio.RecordingOptionsPresets.HIGH_QUALITY
+      );
+      setRecording(recording);
+      console.log('Recording started');
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  }
+
+  async function stopRecording() {
+    console.log('Stopping recording..');
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+    await Audio.setAudioModeAsync({
+      allowsRecordingIOS: false,
+    });
+    const uri = recording.getURI();
+    setLatestUri(uri)
+    console.log('Recording stopped and stored at', uri);
+  }
+
   async function onSubmitAudio(event) {
     console.log("Audio submitted")
-    audio_file = "samantha.mp3"
     event.preventDefault()
 
     try {
-      const response = await transcribeAudio({filename: audio_file})
+      const response = await transcribeAudio({uri: latestUri})
       const data = await response;
       if (response == undefined) {
         throw new Error("Audio request failed")
@@ -69,6 +102,8 @@ export default function App() {
       />
       <Button style={styles.button} title="Submit" onPress={onSubmit}/>
       <Text style={styles.result}>{result}</Text>
+      <Button style={styles.button} title="Begin Recording" onPress={startRecording}/>
+      <Button style={styles.button} title="End Recording" onPress={stopRecording}/>
       <Button style={styles.button} title="Transcribe audio" onPress={onSubmitAudio}/>
       <Text style={styles.result}>{transcript}</Text>
       <StatusBar style="auto" />
--- api/whisper.js
+++ api/whisper.js
@@ -2,6 +2,11 @@ import { Configuration, OpenAIApi } from "openai";
 import { OPENAI_API_KEY } from "@env";
 const samantha = require("./samantha.mp3")
 import * as FileSystem from 'expo-file-system';
+import { Asset } from 'expo-asset';
+import { Audio } from "expo-av";
+import { Buffer, Blob } from 'buffer';
+import {decode as atob, encode as btoa} from 'base-64'
+
 
 const configuration = new Configuration({
   apiKey: OPENAI_API_KEY,
@@ -23,49 +28,168 @@ export const transcribeAudio = async (req) =>  {
     return;
   }
 
-  const filename = req.filename || '';
+  const uri = req.uri || '';
 //   const audio_file = readFile(filename)
 
-  console.log("filename: " + filename)
-  if (audio_file) {
-    console.log("Audio exists")
+  console.log("uri: " + uri)
+  if (uri) {
+    console.log("Audio exists ")
   } else {
     console.log("Audio does not exist")
     return
   }
 
-  try {
+//   try {
     // console.log("pre fetch")
     // blob = await fetch(filename)
     // console.log("past fetch")
-    console.log(samantha)
+    // console.log(uri)
     // const data = await FileSystem.readAsStringAsync("file://test.txt")
-    const transcript = await openai.createTranscription(samantha, "whisper-1")
+    // const [{ localUri }] = await Asset.loadAsync(
+    //     require("./samantha.mp3")
+    // );
+
+    // console.log("asset = " + await localUri)
+    // "data:audio/mp3;base64," +
+    // base64 = await FileSystem.readAsStringAsync(uri, { encoding: FileSystem.EncodingType.Base64 })
+		// console.log(base64.substring(0,10))
+    // const buffer = Buffer.from(recordingBase64, "base64")
+    // const blob = new Blob([buffer], { type:'audio/mp4' })
+    // const file = new File([blob],'test.mp4', {type:'audio/mp4'})
+    // blob = await createBlob(base64)
+		uriFetch = await fetch(uri)
+		uriBlob = await uriFetch.blob()
+    console.log("blob")
+    console.log(uriBlob.type)
+    const file = new File([uriBlob],'test.m4a', {type: uriBlob.type})
+    // const file = new File([blob], "input.wav", { type: "audio/wav" });
+    console.log(file.name)
+    // console.log(blob)
+    // blob = base64toBlob(base64, "multipart/form-data")
+    response = await whisperFetch(file)
+		console.log(await response)
+    console.log(await response.json())
+    // return;
+    
+
+    //replay to test
+
+
+    // const transcript = await openai.createTranscription(file, "whisper-1")
+    
+    // console.log('pls')
+    return response;
     // res.message = transcript.data.text
-    res.message = "test"
-    console.log("Response: " + res.message)
-  } catch(error) {
-    // Consider adjusting the error handling logic for your use case
-    if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
-    } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
-    }
-  }
+//   }
+//   catch(error) {
+//     // Consider adjusting the error handling logic for your use case
+//     if (error.response) {
+//       console.error(error.response.status, error.response.data);
+//       res.status(error.response.status).json(error.response.data);
+//     } else {
+//       console.error(`Error with OpenAI API request: ${error.message}`);
+//     }
+//   }
   return res;
 }
 
-function generatePrompt(prompt) {
-  const capitalizedAnimal =
-    prompt[0].toUpperCase() + prompt.slice(1).toLowerCase();
-  return `Respond to this message in conversation
+async function createBlob(base64) {
+//     const file = Buffer.from(
+//         base64,
+//         'base64'
+//       )
+// // now it is Buffer
 
-Message: Hi, how are you?
-Response: Great, thanks! How was your day yesterday?
-Message: ${capitalizedAnimal}
-Response:`;
+//     // const blob = new Blob([file])
+//     return new Blob()
+//     return blob
+    response = await (fetch("data:audio/mp3;base64," + base64))
+    return response.blob()
 }
+
+
+async function whisperFetch(file) {
+    var url = "https://api.openai.com/v1/completions";
+    var bearer = 'Bearer ' + OPENAI_API_KEY
+
+    const formData = new FormData()
+		// formData.append('file', file, "file.m4a");
+		formData.append("prompt", "hello");
+    formData.append("model", "text-davinci-003");
+		console.log(formData)
+    // const header = {
+    //     'Authorization': bearer,
+    //     "Content-Type": "multipart/form-data"
+    // }
+    
+    // const options = {
+    //     method: 'POST',
+    //     headers: header,
+    //     body: form
+    //   }
+    // const requestOptions = {
+    //   method: "POST",
+    //   headers: {
+    //       Authorization: `Bearer ${OPENAI_API_KEY}`,
+		// 			"Content-Type": "application/json",
+    //     },
+    //     body: formData,
+    // };
+    // result = await fetch(url, requestOptions);
+
+
+    result = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Authorization': bearer,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "prompt": "hello",
+            "model": "text-davinci-003",
+        })
+    })
+
+    return result
+}
+
+function base64toBlob(base64Data, contentType) {
+    contentType = contentType || '';
+    var sliceSize = 1024;
+    var byteCharacters = atob(base64Data);
+    var bytesLength = byteCharacters.length;
+    // var slicesCount = Math.ceil(bytesLength / sliceSize);
+    var slicesCount = Math.ceil(bytesLength / sliceSize);
+    var byteArrays = new Array(slicesCount);
+
+    for (var sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
+        var begin = sliceIndex * sliceSize;
+        var end = Math.min(begin + sliceSize, bytesLength);
+
+        var bytes = new Array(end - begin);
+        for (var offset = begin, i = 0; offset < end; ++i, ++offset) {
+            bytes[i] = byteCharacters[offset].charCodeAt(0);
+        }
+        byteArrays[sliceIndex] = new Uint8Array(bytes);
+    }
+    // return byteArrays
+    return new Blob(byteArrays, { type: contentType });
+}
+
+
+// used for testing -- recreates an audio file from a base64 string and plays it
+async function recreateAndPlay(base64) {
+    const newUri = FileSystem.cacheDirectory + "samantha2.mp3"
+    await FileSystem.writeAsStringAsync(newUri, base64, {
+        encoding: FileSystem.EncodingType.Base64,
+    });
+
+    console.log("newUri = " + newUri)
+    const sound = new Audio.Sound()
+    await sound.loadAsync({uri: newUri})
+    await sound.playAsync();
+}
+
 
 
 export default transcribeAudio;
