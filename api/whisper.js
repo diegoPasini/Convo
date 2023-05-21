27048ac882bb5b38900051ab0e7f5c1924716b2a
otaylor2023 <75337325+otaylor2023@users.noreply.github.com>
2023-05-20 21:34:16 -0400

    whisper, ui, correctness border

--- api/whisper.js
+++ api/whisper.js
@@ -14,167 +14,71 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export const transcribeAudio = async (req) =>  {
-    res = {
-        status: {
-          code: 0,
-          error: {
-          message: "",
-          }
-        },
-        result: ""
-    }
-  if (!configuration.apiKey) {
-    console.warn("No API key")
-    return;
-  }
+	res = {
+		status: {
+			code: 0,
+			error: {
+				message: "",
+			}
+		},
+		result: ""
+	}
+	if (!configuration.apiKey) {
+		console.warn("No API key")
+		return;
+	}
 
-  const uri = req.uri || '';
-//   const audio_file = readFile(filename)
-
-  console.log("uri: " + uri)
-  if (uri) {
-    console.log("Audio exists ")
-  } else {
-    console.log("Audio does not exist")
-    return
-  }
-
-//   try {
-    // console.log("pre fetch")
-    // blob = await fetch(filename)
-    // console.log("past fetch")
-    // console.log(uri)
-    // const data = await FileSystem.readAsStringAsync("file://test.txt")
-    // const [{ localUri }] = await Asset.loadAsync(
-    //     require("./samantha.mp3")
-    // );
-
-    // console.log("asset = " + await localUri)
-    // "data:audio/mp3;base64," +
-    // base64 = await FileSystem.readAsStringAsync(uri, { encoding: FileSystem.EncodingType.Base64 })
-		// console.log(base64.substring(0,10))
-    // const buffer = Buffer.from(recordingBase64, "base64")
-    // const blob = new Blob([buffer], { type:'audio/mp4' })
-    // const file = new File([blob],'test.mp4', {type:'audio/mp4'})
-    // blob = await createBlob(base64)
-		uriFetch = await fetch(uri)
-		uriBlob = await uriFetch.blob()
-    console.log("blob")
-    console.log(uriBlob.type)
-    const file = new File([uriBlob],'test.m4a', {type: uriBlob.type})
-    // const file = new File([blob], "input.wav", { type: "audio/wav" });
-    console.log(file.name)
-    // console.log(blob)
-    // blob = base64toBlob(base64, "multipart/form-data")
-    response = await whisperFetch(file)
-		console.log(await response)
-    console.log(await response.json())
-    // return;
-    
-
-    //replay to test
-
-
-    // const transcript = await openai.createTranscription(file, "whisper-1")
-    
-    // console.log('pls')
-    return response;
-    // res.message = transcript.data.text
-//   }
-//   catch(error) {
-//     // Consider adjusting the error handling logic for your use case
-//     if (error.response) {
-//       console.error(error.response.status, error.response.data);
-//       res.status(error.response.status).json(error.response.data);
-//     } else {
-//       console.error(`Error with OpenAI API request: ${error.message}`);
-//     }
-//   }
-  return res;
-}
-
-async function createBlob(base64) {
-//     const file = Buffer.from(
-//         base64,
-//         'base64'
-//       )
-// // now it is Buffer
-
-//     // const blob = new Blob([file])
-//     return new Blob()
-//     return blob
-    response = await (fetch("data:audio/mp3;base64," + base64))
-    return response.blob()
+	const uri = req.uri || '';
+	if (!uri) {
+		console.error("Audio does not exist")
+		return
+	}
+	
+	try {
+		response = await whisperFetch(uri)
+		// console.log(response)
+		resJson = await response.json()
+		console.log(resJson)
+		return resJson;
+	} catch(error) {
+	  // Consider adjusting the error handling logic for your use case
+	  if (error.response) {
+		console.error(error.response.status, error.response.data);
+		res.status(error.response.status).json(error.response.data);
+	  } else {
+		console.error(`Error with OpenAI API request: ${error.message}`);
+	  }
+	}
 }
 
 
-async function whisperFetch(file) {
-    var url = "https://api.openai.com/v1/completions";
-    var bearer = 'Bearer ' + OPENAI_API_KEY
+async function whisperFetch(uri) {
+	var url = "https://api.openai.com/v1/audio/transcriptions";
+	var bearer = 'Bearer ' + OPENAI_API_KEY
+	const formData = new FormData()
 
-    const formData = new FormData()
-		// formData.append('file', file, "file.m4a");
-		formData.append("prompt", "hello");
-    formData.append("model", "text-davinci-003");
-		console.log(formData)
-    // const header = {
-    //     'Authorization': bearer,
-    //     "Content-Type": "multipart/form-data"
-    // }
-    
-    // const options = {
-    //     method: 'POST',
-    //     headers: header,
-    //     body: form
-    //   }
-    // const requestOptions = {
-    //   method: "POST",
-    //   headers: {
-    //       Authorization: `Bearer ${OPENAI_API_KEY}`,
-		// 			"Content-Type": "application/json",
-    //     },
-    //     body: formData,
-    // };
-    // result = await fetch(url, requestOptions);
+	formData.append('file', {
+		uri: uri,
+		type: 'audio/mp4',
+		name: 'audio.m4a'
+	});
 
+	formData.append("model", "whisper-1");
+	formData.append("language", "es")
 
-    result = await fetch(url, {
-        method: 'POST',
-        headers: {
-            'Authorization': bearer,
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            "prompt": "hello",
-            "model": "text-davinci-003",
-        })
-    })
+	result = await fetch(url, {
+		method: 'POST',
+		headers: {
+				'Authorization': bearer,
+				"Content-Type": "multipart/form-data"
+		},
+		body: formData
+})
 
-    return result
+	return result
 }
 
-function base64toBlob(base64Data, contentType) {
-    contentType = contentType || '';
-    var sliceSize = 1024;
-    var byteCharacters = atob(base64Data);
-    var bytesLength = byteCharacters.length;
-    // var slicesCount = Math.ceil(bytesLength / sliceSize);
-    var slicesCount = Math.ceil(bytesLength / sliceSize);
-    var byteArrays = new Array(slicesCount);
 
-    for (var sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
-        var begin = sliceIndex * sliceSize;
-        var end = Math.min(begin + sliceSize, bytesLength);
-
-        var bytes = new Array(end - begin);
-        for (var offset = begin, i = 0; offset < end; ++i, ++offset) {
-            bytes[i] = byteCharacters[offset].charCodeAt(0);
-        }
-        byteArrays[sliceIndex] = new Uint8Array(bytes);
-    }
-    // return byteArrays
-    return new Blob(byteArrays, { type: contentType });
-}
 
 
 // used for testing -- recreates an audio file from a base64 string and plays it
